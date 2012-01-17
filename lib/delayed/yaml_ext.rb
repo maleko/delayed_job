83e285e6d18a20de2a7b25398c09e5c5535510a6
Marc Lee <marc.lee@adviso.com.au>
2012-01-17 22:17:55 +1100

    adding Yecht as one of the parsers.

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -5,6 +5,8 @@ require 'yaml'
 if YAML.parser.class.name =~ /syck/i
   require File.expand_path('../syck_ext', __FILE__)
   require File.expand_path('../serialization/active_record', __FILE__)
+elsif YAML.parser.class.name =~ /yecht/i
+  require File.expand_path('../serialization/active_record', __FILE__)
 else
   require File.expand_path('../psych_ext', __FILE__)
 end
